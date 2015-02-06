9b311b62ab0b4795dfe08e22e771f5e7c889195f
wcwung <williamcwung@gmail.com>
2015-02-05 22:19:07 -0800

    Complete linkedlist

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -37,3 +37,4 @@ Queue.prototype.size = function() {
 };
 
 
+
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,59 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(!list.head){
+      list.head = Node(value);
+      list.tail = list.head;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
+    //One node case:
+    //Build a node
+    //tail will = node.value
+    //head will = node.value
+    //More nodes case:
+    //Build a node
+    //***node.value = value
+    //***node.next = null, previous node.next = current node.value;
+    //tail will = lastBuiltNode.value
+    //head will be same
   };
 
   list.removeHead = function(){
+    //store the list.head.value
+    //list.head.value = list.head.next
+    //return list.head.value
+    var headNode = list.head;
+    list.head = headNode.next;
+
+    return headNode.value;
   };
 
   list.contains = function(target){
+    //received the currentNode
+
+    var currentNode = arguments[1] || list.head;
+    var bool;
+    //assigned that currentNode to our currentNode
+
+
+    if(target === currentNode.value){
+      return true;
+    }else if(currentNode.next === null){
+      return false;
+    }else{
+      bool = list.contains(target, currentNode.next);
+    }
+    return bool;
+
+    // Check if target = current node.value: true ? return true
+    // Else, move forward and point to next node
+    // Also check if current Node.next === null
+    // if current Node.next === null, return false for no matching target
+    //
+    // Recursion : call list.contains(target, current node)
+
   };
 
   return list;
