4218ce52dbc129d413882341bd4055254ecaf764
wcwung <williamcwung@gmail.com>
2015-02-06 19:34:18 -0800

    Complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,43 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  // push item into storage
+  // check the storage if the item already exist
+  if(this._storage.indexOf(item) < 0){
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item){
+  //search storage to see if items is in our storage
+  // var bool = false;
+  // for(var i = 0; i < this._storage.length; i++){
+  //   if(item === this._storage[i]){
+  //     bool = true;
+  //   }
+  // }
+  // return bool;
+
+  if(this._storage.indexOf(item) > -1){
+    return true;
+  }else{
+    return false;
+  }
+
+
 };
 
 setPrototype.remove = function(item){
+  if(this._storage.indexOf(item)> -1){
+    delete this._storage[this._storage.indexOf(item)];
+  }
+  //remove the item from storage
 };
 
 /*
