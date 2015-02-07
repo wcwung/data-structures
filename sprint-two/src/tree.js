b657d37925a64a5e14b7d33187bd4d2713d72b6b
wcwung <williamcwung@gmail.com>
2015-02-06 17:40:51 -0800

    Start on tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,22 +3,36 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  //child = Tree(value)
+  var child = Tree(value);
+  if(this.children.length < 4){ //3 children
+    this.children.push(child);
+  }
+  //check the size of .children.length if less than whatever
+  //push the child in to newTree.children
 
 };
 
 treeMethods.contains = function(target){
+  //base perfect case: FOUND (target === value) return true;
+  if (target === this.value) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    this.contains(target, this.children[i]);
+  }
+  //forloop to iterate through the children.
+  //recursivly go through each children.
+  //
 
 };
 
